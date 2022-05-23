cb3c918055dfd5a4b497f3ddbaadc7a0926391bc
Navaneeth P R <rgnavaneeth@gmail.com>
2022-05-23 20:06:57 +0530

    added zigzag recursion

---
+++ DSA_Java/Recursion/zigzag.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class zigzag {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pzz(n);
+    }
+    public static void pzz(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.println("Pre " + n);
+        pzz(n-1);
+        System.out.println("In" + n);
+        pzz(n-1);
+        System.out.println("Post " + n);
+    }
+}
